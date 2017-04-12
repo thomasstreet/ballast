e4bd34e461ebf86f210985ec729b7c3fa9e08027
Justin Smith <smith.justin.c@gmail.com>
2017-04-12 16:43:10 -0700

    Service HTTP request methods implemented

--- balast/service.py
+++ balast/service.py
@@ -1,71 +1,229 @@
 import logging
 import requests
-from exception import BalastException
+from requests.exceptions import RequestException
+from util import UrlBuilder
 
 
 class Service(object):
 
-    _SERVICE_ADDRESS = '{}:{}'
-    _BASE_URL_FORMAT = 'http://{}'
+    DEFAULT_REQUEST_TIMEOUT = 10
 
-    def __init__(self, load_balancer):
+    def __init__(self, load_balancer, use_https=False, request_timeout=DEFAULT_REQUEST_TIMEOUT):
         self._load_balancer = load_balancer
-        self._logger = logging.getLogger(self.__class__.__name__)
+        self._use_https = use_https
+        self._request_timeout = request_timeout
+        self._logger = logging.getLogger(self.__module__)
+
+    def request(self, method, url, **kwargs):
+
+        # choose a server from the pool
+        server = self._load_balancer.choose_server()
+        absolute_url = self._get_absolute_url(server, url, self._use_https)
+
+        self._logger.debug("Request: %s %s", method.upper(), absolute_url)
+
+        try:
+            response = requests.request(method, absolute_url, timeout=self._request_timeout, **kwargs)
+
+            # 5xx errors should mark the server down
+            # everything else is good to go
+            if response.status_code < 500:
+                return response
+
+        except RequestException as e:
+            self._logger.error("Request to server failed for url: '%s': %s", absolute_url, e)
+
+        # mark this server down and try the
+        # request again with a new server
+        self._load_balancer.mark_server_down(server)
+
+        return self.request(method, url, **kwargs)
 
     def options(self, url, **kwargs):
-        # TODO: if fails to connect, mark server down, choose again
-        absolute_url, server = self._get_absolute_url(url)
-        return requests.options(absolute_url, **kwargs)
+
+        # choose a server from the pool
+        server = self._load_balancer.choose_server()
+        absolute_url = self._get_absolute_url(server, url, self._use_https)
+
+        self._logger.debug("Request: OPTIONS %s", absolute_url)
+
+        try:
+            response = requests.options(absolute_url, timeout=self._request_timeout, **kwargs)
+
+            # 5xx errors should mark the server down
+            # everything else is good to go
+            if response.status_code < 500:
+                return response
+
+        except RequestException as e:
+            self._logger.error("Request to server failed for url: '%s': %s", absolute_url, e)
+
+        # mark this server down and try the
+        # request again with a new server
+        self._load_balancer.mark_server_down(server)
+
+        return self.options(url, **kwargs)
 
     def head(self, url, **kwargs):
-        # TODO: if fails to connect, mark server down, choose again
-        absolute_url, server = self._get_absolute_url(url)
-        return requests.head(absolute_url, **kwargs)
+
+        # choose a server from the pool
+        server = self._load_balancer.choose_server()
+        absolute_url = self._get_absolute_url(server, url, self._use_https)
+
+        self._logger.debug("Request: HEAD %s", absolute_url)
+
+        try:
+            response = requests.head(absolute_url, timeout=self._request_timeout, **kwargs)
+
+            # 5xx errors should mark the server down
+            # everything else is good to go
+            if response.status_code < 500:
+                return response
+
+        except RequestException as e:
+            self._logger.error("Request to server failed for url: '%s': %s", absolute_url, e)
+
+        # mark this server down and try the
+        # request again with a new server
+        self._load_balancer.mark_server_down(server)
+
+        return self.head(url, **kwargs)
 
     def get(self, url, params=None, **kwargs):
-        # TODO: if fails to connect, mark server down, choose again
-        absolute_url, server = self._get_absolute_url(url)
-        return requests.get(absolute_url, params, **kwargs)
+
+        # choose a server from the pool
+        server = self._load_balancer.choose_server()
+        absolute_url = self._get_absolute_url(server, url, self._use_https)
+
+        self._logger.debug("Request: GET %s", absolute_url)
+
+        try:
+            response = requests.get(absolute_url, params, timeout=self._request_timeout, **kwargs)
+
+            # 5xx errors should mark the server down
+            # everything else is good to go
+            if response.status_code < 500:
+                return response
+
+        except RequestException as e:
+            self._logger.error("Request to server failed for url: '%s': %s", absolute_url, e)
+
+        # mark this server down and try the
+        # request again with a new server
+        self._load_balancer.mark_server_down(server)
+
+        return self.get(url, params, **kwargs)
 
     def post(self, url, data=None, json=None, **kwargs):
-        absolute_url, server = self._get_absolute_url(url)
+
+        # choose a server from the pool
+        server = self._load_balancer.choose_server()
+        absolute_url = self._get_absolute_url(server, url, self._use_https)
+
+        self._logger.debug("Request: POST %s", absolute_url)
+
         try:
-            return requests.post(absolute_url, data, json, timeout=10, **kwargs)
-        except BaseException:
-            self._load_balancer.mark_server_down(server)
-            return self.post(url, data, json, **kwargs)
+            response = requests.post(absolute_url, data, json, timeout=self._request_timeout, **kwargs)
+
+            # 5xx errors should mark the server down
+            # everything else is good to go
+            if response.status_code < 500:
+                return response
+
+        except RequestException as e:
+            self._logger.error("Request to server failed for url: '%s': %s", absolute_url, e)
+
+        # mark this server down and try the
+        # request again with a new server
+        self._load_balancer.mark_server_down(server)
+
+        return self.post(url, data, json, **kwargs)
 
     def put(self, url, data=None, **kwargs):
-        # TODO: if fails to connect, mark server down, choose again
-        absolute_url, server = self._get_absolute_url(url)
-        return requests.put(absolute_url, data, **kwargs)
 
-    def patch(self, url, data=None, **kwargs):
-        # TODO: if fails to connect, mark server down, choose again
-        absolute_url, server = self._get_absolute_url(url)
-        return requests.patch(absolute_url, data, **kwargs)
+        # choose a server from the pool
+        server = self._load_balancer.choose_server()
+        absolute_url = self._get_absolute_url(server, url, self._use_https)
 
-    def delete(self, url, **kwargs):
-        # TODO: if fails to connect, mark server down, choose again
-        absolute_url, server = self._get_absolute_url(url)
-        return requests.delete(absolute_url, **kwargs)
-
-    def _get_service_address(self):
+        self._logger.debug("Request: PUT %s", absolute_url)
 
         try:
-            server = self._load_balancer.choose_server()
-            return self._SERVICE_ADDRESS.format(
-                server.address,
-                server.port
-            ), server
-        except BaseException as e:
-            if isinstance(e, BalastException):
-                raise e
-            else:
-                raise BalastException(cause=e)
+            response = requests.put(absolute_url, data, timeout=self._request_timeout, **kwargs)
 
-    def _get_absolute_url(self, url):
-        service_address, server = self._get_service_address()
-        base_url = self._BASE_URL_FORMAT.format(service_address)
+            # 5xx errors should mark the server down
+            # everything else is good to go
+            if response.status_code < 500:
+                return response
 
-        return base_url + url, server
+        except RequestException as e:
+            self._logger.error("Request to server failed for url: '%s': %s", absolute_url, e)
+
+        # mark this server down and try the
+        # request again with a new server
+        self._load_balancer.mark_server_down(server)
+
+        return self.put(url, data, **kwargs)
+
+    def patch(self, url, data=None, **kwargs):
+
+        # choose a server from the pool
+        server = self._load_balancer.choose_server()
+        absolute_url = self._get_absolute_url(server, url, self._use_https)
+
+        self._logger.debug("Request: PATCH %s", absolute_url)
+
+        try:
+            response = requests.patch(absolute_url, data, timeout=self._request_timeout, **kwargs)
+
+            # 5xx errors should mark the server down
+            # everything else is good to go
+            if response.status_code < 500:
+                return response
+
+        except RequestException as e:
+            self._logger.error("Request to server failed for url: '%s': %s", absolute_url, e)
+
+        # mark this server down and try the
+        # request again with a new server
+        self._load_balancer.mark_server_down(server)
+
+        return self.patch(url, data, **kwargs)
+
+    def delete(self, url, **kwargs):
+
+        # choose a server from the pool
+        server = self._load_balancer.choose_server()
+        absolute_url = self._get_absolute_url(server, url, self._use_https)
+
+        self._logger.debug("Request: DELETE %s", absolute_url)
+
+        try:
+            response = requests.delete(absolute_url, timeout=self._request_timeout, **kwargs)
+
+            # 5xx errors should mark the server down
+            # everything else is good to go
+            if response.status_code < 500:
+                return response
+
+        except RequestException as e:
+            self._logger.error("Request to server failed for url: '%s': %s", absolute_url, e)
+
+        # mark this server down and try the
+        # request again with a new server
+        self._load_balancer.mark_server_down(server)
+
+        return self.delete(url, **kwargs)
+
+    @staticmethod
+    def _get_absolute_url(server, relative_url, use_https):
+
+        url = UrlBuilder.from_parts(
+            hostname=server.address,
+            port=server.port
+        )
+        url.append_path(relative_url)
+
+        if use_https:
+            url.https()
+
+        return unicode(url)
---
+++ test/test_service.py
@@ -0,0 +1,242 @@
+import unittest
+import mock
+from requests import models, exceptions
+from balast import ping, Service, LoadBalancer
+from balast.util import UrlBuilder
+from balast.discovery.static import StaticServerList
+from balast.exception import NoReachableServersException
+
+
+_EXPECTED_SERVERS = ['127.0.0.1', '127.0.0.2']
+
+
+class _MockResponse(models.Response):
+
+    def __init__(self, status_code):
+        super(_MockResponse, self).__init__()
+        self.status_code = status_code
+
+
+class ServiceTest(unittest.TestCase):
+
+    def setUp(self):
+        self._service = self._create_service()
+        self._load_balancer = self._service._load_balancer
+        self._servers = self._load_balancer.servers
+
+    @mock.patch('balast.service.requests.request', return_value=_MockResponse(200))
+    def test_request_ok(self, mock_request):
+
+        # make our request
+        result = self._service.request('GET', '/relative/path')
+
+        # should have tried twice - once for each server
+        self.assertEqual(1, mock_request.call_count)
+        self.assertEqual(result, mock_request.return_value)
+
+        # verify the url we create
+        call_arg = mock_request.call_args_list[0]
+        url = UrlBuilder.from_url(call_arg[0][1])
+        self.assertIn(url._hostname, _EXPECTED_SERVERS)
+        self.assertEqual(url._scheme, 'https')
+        self.assertIsNone(url._port)
+        self.assertEqual(url._path, '/relative/path')
+
+    @mock.patch('balast.service.requests.head', return_value=_MockResponse(200))
+    def test_head_ok(self, mock_request):
+        self.assert_request_ok(mock_request, self._service.head)
+
+    @mock.patch('balast.service.requests.options', return_value=_MockResponse(200))
+    def test_options_ok(self, mock_request):
+        self.assert_request_ok(mock_request, self._service.options)
+
+    @mock.patch('balast.service.requests.get', return_value=_MockResponse(200))
+    def test_get_ok(self, mock_request):
+        self.assert_request_ok(mock_request, self._service.get)
+
+    @mock.patch('balast.service.requests.post', return_value=_MockResponse(200))
+    def test_post_ok(self, mock_request):
+        self.assert_request_ok(mock_request, self._service.post)
+
+    @mock.patch('balast.service.requests.put', return_value=_MockResponse(200))
+    def test_put_ok(self, mock_request):
+        self.assert_request_ok(mock_request, self._service.put)
+
+    @mock.patch('balast.service.requests.patch', return_value=_MockResponse(200))
+    def test_patch_ok(self, mock_request):
+        self.assert_request_ok(mock_request, self._service.patch)
+
+    @mock.patch('balast.service.requests.delete', return_value=_MockResponse(200))
+    def test_delete_ok(self, mock_request):
+        self.assert_request_ok(mock_request, self._service.delete)
+
+    @mock.patch('balast.service.requests.request', return_value=_MockResponse(500))
+    def test_all_request_servers_error(self, mock_request):
+
+        mock_request.side_effect = exceptions.RequestException('mock exception')
+
+        # will try each server, and mark each down on a 500 response
+        # when no servers are left, a NoReachableServersException is thrown
+        self.assertRaises(NoReachableServersException, self._service.request, 'GET', '/relative/path')
+
+        # should have tried twice - once for each server
+        self.assertEqual(2, mock_request.call_count)
+
+        # add our hostnames to a set (to ensure the
+        # same server isn't being used repeatedly)
+        # all other info should be the same for each server request
+        hostnames = set()
+        for i, call_arg in enumerate(mock_request.call_args_list):
+            url = UrlBuilder.from_url(call_arg[0][1])
+            hostnames.add(url._hostname)
+            self.assertEqual(url._scheme, 'https')
+            self.assertIsNone(url._port)
+            self.assertEqual(url._path, '/relative/path')
+
+        # less than 2 means a server has been reused
+        self.assertEqual(2, len(hostnames))
+        for hostname in hostnames:
+            self.assertIn(hostname, _EXPECTED_SERVERS)
+
+    @mock.patch('balast.service.requests.options', return_value=_MockResponse(500))
+    def test_all_options_servers_error(self, mock_request):
+        mock_request.side_effect = exceptions.RequestException('mock exception')
+        self.assert_all_servers_fail(mock_request, self._service.options)
+
+    @mock.patch('balast.service.requests.head', return_value=_MockResponse(500))
+    def test_all_head_servers_error(self, mock_request):
+        mock_request.side_effect = exceptions.RequestException('mock exception')
+        self.assert_all_servers_fail(mock_request, self._service.head)
+
+    @mock.patch('balast.service.requests.get', return_value=_MockResponse(500))
+    def test_all_get_servers_error(self, mock_request):
+        mock_request.side_effect = exceptions.RequestException('mock exception')
+        self.assert_all_servers_fail(mock_request, self._service.get)
+
+    @mock.patch('balast.service.requests.post', return_value=_MockResponse(500))
+    def test_all_post_servers_error(self, mock_request):
+        mock_request.side_effect = exceptions.RequestException('mock exception')
+        self.assert_all_servers_fail(mock_request, self._service.post)
+
+    @mock.patch('balast.service.requests.put', return_value=_MockResponse(500))
+    def test_all_put_servers_error(self, mock_request):
+        mock_request.side_effect = exceptions.RequestException('mock exception')
+        self.assert_all_servers_fail(mock_request, self._service.put)
+
+    @mock.patch('balast.service.requests.patch', return_value=_MockResponse(500))
+    def test_all_patch_servers_error(self, mock_request):
+        mock_request.side_effect = exceptions.RequestException('mock exception')
+        self.assert_all_servers_fail(mock_request, self._service.patch)
+
+    @mock.patch('balast.service.requests.delete', return_value=_MockResponse(500))
+    def test_all_delete_servers_error(self, mock_request):
+        mock_request.side_effect = exceptions.RequestException('mock exception')
+        self.assert_all_servers_fail(mock_request, self._service.delete)
+
+    @mock.patch('balast.service.requests.request', return_value=_MockResponse(500))
+    def test_all_request_servers_500(self, mock_request):
+
+        # will try each server, and mark each down on a 500 response
+        # when no servers are left, a NoReachableServersException is thrown
+        self.assertRaises(NoReachableServersException, self._service.request, 'GET', '/relative/path')
+
+        # should have tried twice - once for each server
+        self.assertEqual(2, mock_request.call_count)
+
+        # add our hostnames to a set (to ensure the
+        # same server isn't being used repeatedly)
+        # all other info should be the same for each server request
+        hostnames = set()
+        for i, call_arg in enumerate(mock_request.call_args_list):
+            url = UrlBuilder.from_url(call_arg[0][1])
+            hostnames.add(url._hostname)
+            self.assertEqual(url._scheme, 'https')
+            self.assertIsNone(url._port)
+            self.assertEqual(url._path, '/relative/path')
+
+        # less than 2 means a server has been reused
+        self.assertEqual(2, len(hostnames))
+        for hostname in hostnames:
+            self.assertIn(hostname, _EXPECTED_SERVERS)
+
+    @mock.patch('balast.service.requests.options', return_value=_MockResponse(500))
+    def test_all_options_servers_500(self, mock_request):
+        self.assert_all_servers_fail(mock_request, self._service.options)
+
+    @mock.patch('balast.service.requests.head', return_value=_MockResponse(500))
+    def test_all_head_servers_500(self, mock_request):
+        self.assert_all_servers_fail(mock_request, self._service.head)
+
+    @mock.patch('balast.service.requests.get', return_value=_MockResponse(500))
+    def test_all_get_servers_500(self, mock_request):
+        self.assert_all_servers_fail(mock_request, self._service.get)
+
+    @mock.patch('balast.service.requests.post', return_value=_MockResponse(500))
+    def test_all_post_servers_500(self, mock_request):
+        self.assert_all_servers_fail(mock_request, self._service.post)
+
+    @mock.patch('balast.service.requests.put', return_value=_MockResponse(500))
+    def test_all_put_servers_500(self, mock_request):
+        self.assert_all_servers_fail(mock_request, self._service.put)
+
+    @mock.patch('balast.service.requests.patch', return_value=_MockResponse(500))
+    def test_all_patch_servers_500(self, mock_request):
+        self.assert_all_servers_fail(mock_request, self._service.patch)
+
+    @mock.patch('balast.service.requests.delete', return_value=_MockResponse(500))
+    def test_all_delete_servers_500(self, mock_request):
+        self.assert_all_servers_fail(mock_request, self._service.delete)
+
+    def assert_request_ok(self, mock_request, request_call):
+
+        # make our request
+        result = request_call('/relative/path')
+
+        # should have tried twice - once for each server
+        self.assertEqual(1, mock_request.call_count)
+        self.assertEqual(result, mock_request.return_value)
+
+        # verify the url we create
+        call_arg = mock_request.call_args_list[0]
+        url = UrlBuilder.from_url(call_arg[0][0])
+        self.assertIn(url._hostname, _EXPECTED_SERVERS)
+        self.assertEqual(url._scheme, 'https')
+        self.assertIsNone(url._port)
+        self.assertEqual(url._path, '/relative/path')
+
+    def assert_all_servers_fail(self, mock_request, request_call):
+
+        # will try each server, and mark each down on a 500 response
+        # when no servers are left, a NoReachableServersException is thrown
+        self.assertRaises(NoReachableServersException, request_call, '/relative/path')
+
+        # should have tried twice - once for each server
+        self.assertEqual(2, mock_request.call_count)
+
+        # add our hostnames to a set (to ensure the
+        # same server isn't being used repeatedly)
+        # all other info should be the same for each server request
+        hostnames = set()
+        for i, call_arg in enumerate(mock_request.call_args_list):
+            url = UrlBuilder.from_url(call_arg[0][0])
+            hostnames.add(url._hostname)
+            self.assertEqual(url._scheme, 'https')
+            self.assertIsNone(url._port)
+            self.assertEqual(url._path, '/relative/path')
+
+        # less than 2 means a server has been reused
+        self.assertEqual(2, len(hostnames))
+        for hostname in hostnames:
+            self.assertIn(hostname, _EXPECTED_SERVERS)
+
+    @staticmethod
+    def _create_service():
+
+        servers = StaticServerList(_EXPECTED_SERVERS)
+        load_balancer = LoadBalancer(servers, ping=ping.DummyPing())
+        service = Service(load_balancer, use_https=True, request_timeout=0.1)
+
+        import gevent
+        gevent.sleep()
+
+        return service
