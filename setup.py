acd09349f9f58f4735c96bcd899d9238278054f6
Justin Smith <smith.justin.c@gmail.com>
2017-04-13 02:28:40 -0700

    Updated setup.py for alpha readiness

--- setup.py
+++ setup.py
@@ -108,9 +108,9 @@ setup(name='balast',
           #   3 - Alpha
           #   4 - Beta
           #   5 - Production/Stable
-          "Development Status :: 1 - Planning",
+          # "Development Status :: 1 - Planning",
           # "Development Status :: 2 - Pre-Alpha",
-          # "Development Status :: 3 - Alpha",
+          "Development Status :: 3 - Alpha",
           # "Development Status :: 4 - Beta",
 
           # Indicate who your project is intended for
@@ -122,13 +122,13 @@ setup(name='balast',
 
           # Specify the Python versions you support here. In particular, ensure
           # that you indicate whether you support Python 2, Python 3 or both.
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
+          # 'Programming Language :: Python :: 2',
+          # 'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
+          # 'Programming Language :: Python :: 3',
+          # 'Programming Language :: Python :: 3.2',
+          # 'Programming Language :: Python :: 3.3',
+          # 'Programming Language :: Python :: 3.4',
       ],
       keywords='alpha development load-balancing',
       )
