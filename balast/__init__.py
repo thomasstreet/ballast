cdf300e7ab5a73999f1530867d2f7ec00cb5a70a
Justin Smith <smith.justin.c@gmail.com>
2017-04-15 03:00:07 -0700

    Configured NullHandler for default logging

--- balast/__init__.py
+++ balast/__init__.py
@@ -4,6 +4,16 @@ from rule import Rule
 from discovery import Server, ServerList
 from service import Service
 
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
 __all__ = [
     'LoadBalancer',
     'Ping',
